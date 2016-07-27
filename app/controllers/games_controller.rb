c8906861997ea226d9a84c70de115b0ad02bb872
Janet Mohler <janetmohler82@gmail.com>
2016-07-27 22:14:04 +0000

    Adding the games/create action

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,12 +1,25 @@
 class GamesController < ApplicationController
 
   def new
+    @game = Game.new
   end
 
   def  create
+    @game = current_user.games.create(game_params)
+    if @game.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:name, :white_user_id, :black_user_id, :winning_user_id)
   end
 
 end
