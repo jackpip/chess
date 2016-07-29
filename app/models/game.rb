4c5934e6ca29e5c37b3e1b5afaad066ca6c49989
MarcMcArthur <mcarthur.marcrichard@gmail.com>
2016-07-29 23:20:10 +0000

    Added populate board method and rspec test for correct count and position of pieces

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,6 +4,46 @@ class Game < ActiveRecord::Base
   has_many :moves
   has_many :pieces
 
+  after_create :populate_board!
+
+  def populate_board!
+
+    # Black Pieces
+    (0..7).each do |i|
+      Pawn.create(game_id: id, current_position_x: i, current_position_y: 1, color: :black)
+    end
+
+    Rook.create(game_id: id, current_position_x: 0, current_position_y: 0, color: :black)
+    Rook.create(game_id: id, current_position_x: 7, current_position_y: 0, color: :black)
+
+    Knight.create(game_id: id, current_position_x: 1, current_position_y: 0, color: :black)
+    Knight.create(game_id: id, current_position_x: 6, current_position_y: 0, color: :black)
+
+    Bishop.create(game_id: id, current_position_x: 2, current_position_y: 0, color: :black)
+    Bishop.create(game_id: id, current_position_x: 5, current_position_y: 0, color: :black)
+
+    Queen.create(game_id: id, current_position_x: 3, current_position_y: 0, color: :black)
+    King.create(game_id: id, current_position_x: 4, current_position_y: 0, color: :black)
+
+    # White Pieces
+    (0..7).each do |i|
+      Pawn.create(game_id: id, current_position_x: i, current_position_y: 6, color: :white)
+    end
+
+    Rook.create(game_id: id, current_position_x: 0, current_position_y: 7, color: :white)
+    Rook.create(game_id: id, current_position_x: 7, current_position_y: 7, color: :white)
+
+    Knight.create(game_id: id, current_position_x: 1, current_position_y: 7, color: :white)
+    Knight.create(game_id: id, current_position_x: 6, current_position_y: 7, color: :white)
+
+    Bishop.create(game_id: id, current_position_x: 2, current_position_y: 7, color: :white)
+    Bishop.create(game_id: id, current_position_x: 5, current_position_y: 7, color: :white)
+
+    Queen.create(game_id: id, current_position_x: 3, current_position_y: 7, color: :white)
+    King.create(game_id: id, current_position_x: 4, current_position_y: 7, color: :white)
+
+  end
+
   def is_obstructed?(current_piece_id, x, y)
     piece = Piece.find(current_piece_id)
     current_x = piece.current_position_x
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,50 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
+RSpec.describe Game, type: :model do
+  describe "populate_board!" do
+    it "it should populate board with 32 pieces" do
+  
+      g = Game.create
+      expect(g.pieces.count).to eq(32)
 
+    end
+
+    it "should check that there are 8 Black pieces and they are in the correct position" do
+
+      g = Game.create
+      # check black
+      y1_pieces = g.pieces.where(current_position_y: 1)
+      expect(y1_pieces.count).to eq(8)
+
+      y1_pieces.each_with_index do |piece, index|
+        expect(piece.current_position_x).to eq index
+        expect(piece.type).to eq 'Pawn'
+        expect(piece.color).to eq 'black'
+      end
+
+      y0_pieces = g.pieces.where(current_position_y: 0).order(:current_position_x)
+      expect(y0_pieces.map {|piece| piece.type }).to eq(["Rook", "Knight", "Bishop", "Queen", "King", "Bishop", "Knight", "Rook"])
+      expect(y0_pieces.map {|piece| piece.color }).to eq(["black"]*8)
+
+    end
+
+    it "should check that there are 8 White pieces and they are in the correct position" do
+
+      g = Game.create
+      # check white
+      y6_pieces = g.pieces.where(current_position_y: 6)
+      expect(y6_pieces.count).to eq(8)
+      y6_pieces.each_with_index do |piece, index|
+        expect(piece.current_position_x).to eq index
+        expect(piece.type).to eq 'Pawn'
+        expect(piece.color).to eq 'white'
+      end
+
+      y7_pieces = g.pieces.where(current_position_y: 7).order(:current_position_x)
+      expect(y7_pieces.map {|piece| piece.type }).to eq(["Rook", "Knight", "Bishop", "Queen", "King", "Bishop", "Knight", "Rook"])
+      expect(y7_pieces.map {|piece| piece.color }).to eq(["white"]*8) 
+
+    end
+
+  end
 end
