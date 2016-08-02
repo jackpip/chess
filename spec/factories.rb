9cccdc3f4a8edfea6aa67c0f8cb6b3dd2797ad90
Jack Pipitone <pipitone.jack@gmail.com>
2016-08-02 20:38:49 +0000

    Moved is_obstructed? to piece model and added tests

---
+++ spec/factories.rb
@@ -0,0 +1,13 @@
+FactoryGirl.define do
+
+  factory :game do 
+    name "Hello"
+  end
+
+  factory :piece do
+    current_position_x 4
+    current_position_y 4
+    association :game
+  end
+
+end
