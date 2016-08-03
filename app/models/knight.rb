7e670e267ba7ffe74d68ecd0198f1a065f2b53c4
BJ Voth <bjvoth86@gmail.com>
2016-08-03 20:11:39 +0000

    Create is_valid? method for knight and test

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -1,2 +1,5 @@
 class Knight < Piece
+  def valid_move?(destination_x, destination_y)
+    return (destination_x - current_position_x).abs + (destination_y - current_position_y).abs == 3
+  end
 end
