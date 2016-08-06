4cb26844f0fc4ddd57b296c271023a14ef6c9fcc
emmanueljucaban <emmanueljucaban@gmail.com>
2016-08-06 01:06:29 +0000

    Added is_valid_move to queen

--- app/models/queen.rb
+++ app/models/queen.rb
@@ -1,2 +1,10 @@
 class Queen < Piece
+  def valid_move? (position_x, position_y)
+    if position_x < 0 || position_x > 7 || position_y < 0 || position_y > 7
+      return false
+    elsif ( is_obstructed(position_x, position_y))
+      return false
+    end
+      return true
+    end
 end
