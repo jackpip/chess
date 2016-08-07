1d94d519058d78fce5347e220baa57902cf52393
MarcMcArthur <mcarthur.marcrichard@gmail.com>
2016-08-07 22:33:40 +0000

    Added is_obstructed to is_valid move for bishop

--- app/models/bishop.rb
+++ app/models/bishop.rb
@@ -1,11 +1,12 @@
 class Bishop < Piece
   def is_valid_move?(move_to_x, move_to_y)
 
-    if super == true && (current_position_x - move_to_x).abs == (current_position_y - move_to_y).abs 
+    if is_obstructed?(move_to_x, move_to_y)
+      return false
+    elsif super == true && (current_position_x - move_to_x).abs == (current_position_y - move_to_y).abs 
       return true
     else
       return false
     end
-
   end
 end
--- spec/models/bishop_spec.rb
+++ spec/models/bishop_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Bishop, type: :model do
   game = FactoryGirl.create(:game)
   bishop = FactoryGirl.create(:bishop, current_position_x: 1, current_position_y: 4, color: 'black')
+  bishop1 = FactoryGirl.create(:bishop, current_position_x: 1, current_position_y: 3, color: 'white')
   describe 'is_valid_move?' do
     it 'should return false if move_to square is off board' do
       expect(bishop.is_valid_move?(-1, -3)).to eq(false)
@@ -23,5 +24,9 @@ RSpec.describe Bishop, type: :model do
     it 'should return true when a move is in range for Bishop' do
       expect(bishop.is_valid_move?(2, 3)).to be(true)
     end 
+
+    it 'should return false if square is blocked' do
+      expect(bishop1.is_valid_move?(4, 0)).to eq(false) 
+    end
   end
 end
