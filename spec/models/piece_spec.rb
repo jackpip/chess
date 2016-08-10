ed8326e6f34c2f72925bc907bb16da93d9e743fd
BJ Voth <bjvoth86@gmail.com>
2016-08-10 13:26:29 -0700

    tests obstructed

--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -2,22 +2,36 @@ require 'rails_helper'
 
 RSpec.describe Piece, type: :model do
   game = FactoryGirl.create(:game)
-  piece_1 = FactoryGirl.create(:piece)
-  piece_2 = FactoryGirl.create(:piece, current_position_x: 6, current_position_y: 5)
+  piece_1 = FactoryGirl.create(:piece, current_position_x: 0, current_position_y: 0)
+  piece_2 = FactoryGirl.create(:piece, current_position_x: 0, current_position_y: 1)
   piece_3 = FactoryGirl.create(:piece, current_position_x: 0, current_position_y: 3, color: 'black')
-  describe '.is_obstructed?' do
-    it 'should return nil on invalid input' do
-      expect(piece_1.is_obstructed?(8, 8)).to eq(nil)
+  describe 'obstructed?' do
+    context 'when there is an obstruction' do
+      it 'should return true' do
+        expect(piece_1.obstructed?(0, 2)).to eq(true)
+        expect(piece_1.obstructed?(0, 3)).to eq(true)
+      end
     end
 
-    it 'should return true on obstruction' do
-      expect(piece_1.is_obstructed?(5, 7)).to eq(true)
-    end
-
-    it 'should return false on no obstruction' do
-      expect(piece_1.is_obstructed?(4, 6)).to eq(false)
+    context 'when there is not an obstruction' do
+      it 'should return false' do
+        expect(piece_2.obstructed?(0, 3)).to eq(false)
+      end
     end
   end
+  # describe '.is_obstructed?' do
+  #   it 'should return nil on invalid input' do
+  #     expect(piece_1.is_obstructed?(8, 8)).to eq(nil)
+  #   end
+  #
+  #   it 'should return true on obstruction' do
+  #     expect(piece_1.is_obstructed?(5, 7)).to eq(true)
+  #   end
+  #
+  #   it 'should return false on no obstruction' do
+  #     expect(piece_1.is_obstructed?(4, 6)).to eq(false)
+  #   end
+  # end
 
   describe 'is_valid_move?' do
     it 'should return false if move_to square is off board' do
