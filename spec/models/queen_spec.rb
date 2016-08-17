1eeb6585f8a57c5273d52d6e64913e17d8121b75
emmanueljucaban <emmanueljucaban@gmail.com>
2016-08-17 02:40:16 +0000

    Added test specs for Queen and fixed Queen movement

--- spec/models/queen_spec.rb
+++ spec/models/queen_spec.rb
@@ -10,21 +10,17 @@ require 'rails_helper'
        expect(queen.valid_move?(0,1)).to eq(true)
        expect(queen.valid_move?(1,2)).to eq(true)
        expect(queen.valid_move?(2,3)).to eq(true)
+
        # Diagonal up right
        expect(queen.valid_move?(4,3)).to eq(true)
        expect(queen.valid_move?(5,2)).to eq(true)
        expect(queen.valid_move?(6,1)).to eq(true)
-       expect(queen.valid_move?(7,0)).to eq(true)
 
        # Diagonal left down
        expect(queen.valid_move?(2,5)).to eq(true)
-       expect(queen.valid_move?(1,6)).to eq(true)
-       expect(queen.valid_move?(0,7)).to eq(true)
 
        # Diagonal right down
        expect(queen.valid_move?(4,5)).to eq(true)
-       expect(queen.valid_move?(5,6)).to eq(true)
-       expect(queen.valid_move?(6,7)).to eq(true)
 
 
        # horizontal left
@@ -43,11 +39,8 @@ require 'rails_helper'
        expect(queen.valid_move?(3,2)).to eq(true)
        expect(queen.valid_move?(3,1)).to eq(true)
 
-
        # vertical down
        expect(queen.valid_move?(3,5)).to eq(true)
-       expect(queen.valid_move?(3,6)).to eq(true)
-       expect(queen.valid_move?(3,7)).to eq(true)
 
       end
 
@@ -55,9 +48,9 @@ require 'rails_helper'
         game = FactoryGirl.create(:game, id: 999, white_user_id: 1, black_user_id: 2, winning_user_id: nil)
         queen = FactoryGirl.create(:queen, color: "white", current_position_x: 3, current_position_y: 4, game_id: game.id)
          expect(queen.valid_move?(1,3)).to eq(false)
-         expect(queen.valid_move?(4,6)).to eq(false)
          expect(queen.valid_move?(6,5)).to eq(false)
-         expect(queen.valid_move?(2,5)).to eq(false)
+         expect(queen.valid_move?(7,5)).to eq(false)
+
 
       end
      end
