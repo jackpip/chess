ae611ac32d1990c906770bf98ad803a4a8264957
MarcMcArthur <mcarthur.marcrichard@gmail.com>
2016-08-18 18:35:33 +0000

    More changes to valid_move, obstructed, castling and check

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -1,6 +1,6 @@
 class Knight < Piece
   def valid_move?(destination_x, destination_y)
-    current_position_x != destination_x && current_position_y != destination_y &&
+    super && current_position_x != destination_x && current_position_y != destination_y &&
       (destination_x - current_position_x).abs + (destination_y - current_position_y).abs == 3
   end
 end
