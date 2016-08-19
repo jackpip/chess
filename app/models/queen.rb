7f7e2f0751af65780cd15d339e6e79a3b749394e
emmanueljucaban <emmanueljucaban@gmail.com>
2016-08-19 04:22:04 +0000

    Refactored Queen's valid_move? method to follow best practices

--- app/models/queen.rb
+++ app/models/queen.rb
@@ -2,17 +2,14 @@ class Queen < Piece
   def valid_move?(position_x, position_y)
     if obstructed?(position_x, position_y)
       return false
-    end
-
-    if ((super == true) &&
-      # vertical
-      (current_position_x == position_x) ||
+    else
+      super == true &&
+      (# vertical
+      current_position_x == position_x ||
       # horizontal
-      (current_position_y == position_y) ||
+      current_position_y == position_y ||
       # diagonal
-      ((current_position_y - position_y).abs == (current_position_x - position_x).abs))
-      return true
+      (current_position_y - position_y).abs == (current_position_x - position_x).abs)
     end
-    return false
   end
 end
